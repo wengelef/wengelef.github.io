e5b5747f58abe024d9110fd20c27a9e031ece0b9
Florian Wengelewski <wengelef@gmail.com>
2019-01-03 19:41:38 +0100

    feature (timeline)

--- src/components/CV.vue
+++ src/components/CV.vue
@@ -6,72 +6,32 @@
     </v-toolbar>-->
 
     <v-content>
-      <section>
-        <v-parallax :src="require('@/assets/avatar.jpg')" height="600">
-          <v-layout
-            column
-            fill-height>
-
-            <v-spacer></v-spacer>
-            
-            <v-footer height="auto" class="pa-1" color="rgba(0, 0, 0, 0.8)">
-              <div>
-                <div class="display-2 pl-5 pt-5">Florian Wengelewski</div>
-                <div class="display-1 pl-5 pt-2 pb-2">Android Developer</div>
-              </div>
-              <v-spacer></v-spacer>
-              
-              <v-list 
-                two-line>
-                <v-list-tile>
-                  <v-list-tile-action>
-                    <v-icon color="white">phone</v-icon>
-                  </v-list-tile-action>
-
-                  <v-list-tile-content>
-                    <v-list-tile-title>+49 176 648 50 30 2</v-list-tile-title>
-                    <v-list-tile-sub-title>Mobile</v-list-tile-sub-title>
-                  </v-list-tile-content>
-                </v-list-tile>
-
-                <v-divider inset></v-divider>
-
-                <v-list-tile>
-                  <v-list-tile-action>
-                    <v-icon color="white">mail</v-icon>
-                  </v-list-tile-action>
-
-                  <v-list-tile-content>
-                    <v-list-tile-title>wengelef@gmail.com</v-list-tile-title>
-                    <v-list-tile-sub-title>Personal</v-list-tile-sub-title>
-                  </v-list-tile-content>
-                </v-list-tile>
-
-                <v-divider inset></v-divider>
-
-                <v-list-tile>
-                  <v-list-tile-action>
-                    <v-icon color="white">location_on</v-icon>
-                  </v-list-tile-action>
-
-                  <v-list-tile-content>
-                    <v-list-tile-title>Aachenerstr. 325</v-list-tile-title>
-                    <v-list-tile-sub-title>50931 Koeln</v-list-tile-sub-title>
-                  </v-list-tile-content>
-                </v-list-tile>
-              </v-list>
-            </v-footer>
+      <v-parallax :src="require('@/assets/avatar.jpg')" height="600">
+        <v-container fluid>
+          <v-layout 
+            row
+            fill-height
+            align-end>
+            <v-flex xs12 sm6 class="pl-5">
+              <v-card color="rgba(0, 0, 0, 0.7)">
+                <div>
+                  <div class="display-2 pl-5 pt-5">Florian Wengelewski</div>
+                  <div class="display-1 pl-5 pt-2 pb-2">Android Developer</div>
+                </div>
+              </v-card>
+            </v-flex>
           </v-layout>
-        </v-parallax>
-      </section>
+        </v-container>
+      </v-parallax>
 
       <v-container fluid>
-        <v-layout justify-center row wrap>
+        <v-layout>
           <v-flex text-xs-center>
-            <v-btn round :outline="outlineSkills" color="success" @click="skillsClick">Skills</v-btn>
-            <v-btn round :outline="outlineCurrently" color="error" @click="currentlyClick">Currently</v-btn>
-            <v-btn round :outline="outlinePast" color="warning" @click="pastClick">Past</v-btn>
-            <v-btn round :outline="outlineBacklog" color="info" @click="backlogClick">Backlog</v-btn>
+            <v-btn round :outline="outlineContact" color="green" @click="contactClick">Contact</v-btn>
+            <v-btn round :outline="outlineSkills" color="teal" @click="skillsClick">Skills</v-btn>
+            <v-btn round :outline="outlineCurrently" color="red" @click="currentlyClick">Present</v-btn>
+            <v-btn round :outline="outlinePast" color="orange" @click="pastClick">Past</v-btn>
+            <v-btn round :outline="outlineBacklog" color="blue" @click="backlogClick">Backlog</v-btn>
           </v-flex>
         </v-layout>
       </v-container>
@@ -82,89 +42,152 @@
             v-model="expands" expand>
 
             <v-expansion-panel-content readonly>
+              <v-flex class="pa-4">
+                <v-list 
+                  two-line>
+                  <v-list-tile>
+                    <v-list-tile-action>
+                      <v-icon color="white">phone</v-icon>
+                    </v-list-tile-action>
+
+                    <v-list-tile-content>
+                      <v-list-tile-title>+49 176 648 50 30 2</v-list-tile-title>
+                      <v-list-tile-sub-title>Mobile</v-list-tile-sub-title>
+                    </v-list-tile-content>
+                  </v-list-tile>
+
+                  <v-divider inset></v-divider>
+
+                  <v-list-tile>
+                    <v-list-tile-action>
+                      <v-icon color="white">mail</v-icon>
+                    </v-list-tile-action>
+
+                    <v-list-tile-content>
+                      <v-list-tile-title>wengelef@gmail.com</v-list-tile-title>
+                      <v-list-tile-sub-title>Personal</v-list-tile-sub-title>
+                    </v-list-tile-content>
+                  </v-list-tile>
+
+                  <v-divider inset></v-divider>
+
+                  <v-list-tile>
+                    <v-list-tile-action>
+                      <v-icon color="white">location_on</v-icon>
+                    </v-list-tile-action>
+
+                    <v-list-tile-content>
+                      <v-list-tile-title>Aachenerstr. 325</v-list-tile-title>
+                      <v-list-tile-sub-title>50931 Koeln</v-list-tile-sub-title>
+                    </v-list-tile-content>
+                  </v-list-tile>
+                </v-list>
+              </v-flex>
+            </v-expansion-panel-content>
+
+            <v-expansion-panel-content readonly>
               <v-icon slot="actions" color="transparent"/>
               <div slot="header">Skills</div>
               <v-flex class="pa-4">
                 <ul>
-                    <li v-for="item in skills" :key="item.text">
-                      {{ item.text }}
-                    </li>
-                  </ul>
+                  <li v-for="item in skills" :key="item.text">
+                    {{ item.text }}
+                  </li>
+                </ul>
               </v-flex>
             </v-expansion-panel-content>
 
             <v-expansion-panel-content readonly>
-              <v-icon slot="actions" color="transparent"/>
-              <div slot="header">nextmarkets GmbH</div>
-              <v-flex class="pa-4">
-                <div><font size="4" color="#999999">Android Developer</font></div>
-                <font size="4" color="#999999">January 2017 - PRESENT</font>
-                <v-divider></v-divider>
-                <v-flex class="pa-4">
-                  <ul>
-                    <li v-for="item in nextmarkets" :key="item.text">
-                      {{ item.text }}
-                    </li>
-                  </ul>
-                </v-flex>
-              </v-flex>
+              <v-timeline>
+                <v-timeline-item
+                  fill-dot
+                  right
+                  color="#29B6F6">
+
+                  
+                  <font slot="opposite" size="4" color="#29B6F6">since January 2017</font>
+                  <v-flex class="pa-4">
+                    <div><font size="5" color="#29B6F6">nextmarkets GmbH</font></div>
+                    <div><font size="4" color="#999999">Android Developer</font></div>
+                    <v-divider></v-divider>
+                    <v-flex class="pa-4">
+                      <ul>
+                        <li v-for="item in nextmarkets" :key="item.text">
+                          {{ item.text }}
+                        </li>
+                      </ul>
+                    </v-flex>
+                  </v-flex>
+                </v-timeline-item>
+              </v-timeline>
             </v-expansion-panel-content>
 
             <v-expansion-panel-content readonly>
-              <v-icon slot="actions" color="transparent"/>
-              <div slot="header">RockAByte GmbH</div>
-              <v-flex class="pa-4">
-                <div><font size="4" color="#999999">Android Developer</font></div>
-                <font size="4" color="#999999">January 2015 - January 2017</font>
-                <v-divider></v-divider>
-                <v-flex class="pa-4">
-                  <ul>
-                    <li v-for="item in rab" :key="item.text">
-                      {{ item.text }}
-                    </li>
-                  </ul>
-                  <div>Projects:</div>
-                  <ul>
-                    <li v-for="item in rabProjects" :key="item.text">
-                      {{ item.text }}
-                    </li>
-                  </ul>
-                </v-flex>
-              </v-flex>
-            </v-expansion-panel-content>
+              <v-timeline>
+                <v-timeline-item
+                  fill-dot
+                  right
+                  color="#f44336">
+                  <font slot="opposite" size="4" color="#f44336">January 2015 - January 2017</font>
+                  <v-flex class="pa-4">
+                    <div><font size="5" color="#f44336">RockAByte GmbH</font></div>
+                    <div><font size="4" color="#999999">Android Developer</font></div>
+                    <v-divider></v-divider>
+                    <v-flex class="pa-4">
+                      <ul>
+                        <li v-for="item in rab" :key="item.text">
+                          {{ item.text }}
+                        </li>
+                      </ul>
+                      <div>Projects:</div>
+                      <ul>
+                        <li v-for="item in rabProjects" :key="item.text">
+                          {{ item.text }}
+                        </li>
+                      </ul>
+                    </v-flex>
+                  </v-flex>
+                </v-timeline-item>
 
-            <v-expansion-panel-content readonly>
-              <v-icon slot="actions" color="transparent"/>
-              <div slot="header">Hottgenroth Software GmbH</div>
-              <v-flex class="pa-4">
-                <div><font size="4" color="#999999">Unity Development internship</font></div>
-                <font size="4" color="#999999">September 2014 - December 2014</font>
-                <v-divider></v-divider>
-                <v-flex class="pa-4">
-                  <ul>
-                    <li v-for="item in hottgenroth" :key="item.text">
-                      {{ item.text }}
-                    </li>
-                  </ul>
-                </v-flex>
-              </v-flex>
-            </v-expansion-panel-content>
+                <v-timeline-item
+                  fill-dot
+                  right
+                  color="#FFEE58">
+                  <font slot="opposite" size="4" color="#FFEE58">September 2014 - December 2014</font>
+                  <v-flex class="pa-4">
+                    <div><font size="5" color="#FFEE58">Hottgenroth Software GmbH</font></div>
+                    <div><font size="4" color="#999999">Unity Development internship</font></div>
+                    <v-divider></v-divider>
+                    <v-flex class="pa-4">
+                      <ul>
+                        <li v-for="item in hottgenroth" :key="item.text">
+                          {{ item.text }}
+                        </li>
+                      </ul>
+                    </v-flex>
+                  </v-flex>
+                </v-timeline-item>
 
-            <v-expansion-panel-content readonly>
-              <v-icon slot="actions" color="transparent"/>
-              <div slot="header">Nurogames GmbH</div>
-              <v-flex class="pa-4">
-                <div><font size="4" color="#999999">Android Developer</font></div>
-                <font size="4" color="#999999">March 2012 - September 2013</font>
-                <v-divider></v-divider>
-                <v-flex class="pa-4">
-                  <ul>
-                    <li v-for="item in nurogames" :key="item.text">
-                      {{ item.text }}
-                    </li>
-                  </ul>
-                </v-flex>
-              </v-flex>
+                <v-timeline-item
+                  fill-dot
+                  right
+                  color="#0097A7">
+                  <font slot="opposite" size="4" color="#0097A7">March 2012 - September 2013</font>
+                  <v-flex class="pa-4">
+                    <div><font size="5" color="#0097A7">Nurogames GmbH</font></div>
+                    <div><font size="4" color="#999999">Android Developer</font></div>
+                    <v-divider></v-divider>
+                    <v-flex class="pa-4">
+                      <ul>
+                        <li v-for="item in nurogames" :key="item.text">
+                          {{ item.text }}
+                        </li>
+                      </ul>
+                    </v-flex>
+                  </v-flex>
+                </v-timeline-item>
+
+              </v-timeline>
             </v-expansion-panel-content>
 
             <v-expansion-panel-content readonly>
@@ -191,11 +214,12 @@
 export default {
   name: 'CV',
   data: () => ({
+    outlineContact: true,
     outlineSkills: false,
-    outlineCurrently: true,
+    outlineCurrently: false,
     outlinePast: true,
     outlineBacklog: true,
-    expands: [true, false, false, false, false],
+    expands: [false, true, true, false, false],
     skills: [
       { text: 'Seasoned in using Android Studio IDE for Android Development, Android SDK’s and commonly used third-party-libraries' },
       { text: 'Git version control system' },
@@ -270,43 +294,48 @@ export default {
     currentlyClick: function (event) {
       this.outlineCurrently = !this.outlineCurrently
       this.expands = [
-        !this.outlineSkills, 
-        !this.outlineCurrently, 
-        !this.outlinePast, 
-        !this.outlinePast, 
-        !this.outlinePast, 
+        !this.outlineContact,
+        !this.outlineSkills,
+        !this.outlineCurrently,
+        !this.outlinePast,
         !this.outlineBacklog]
     },
     skillsClick: function (event) {
       this.outlineSkills = !this.outlineSkills
       this.expands = [
-        !this.outlineSkills, 
-        !this.outlineCurrently, 
-        !this.outlinePast, 
-        !this.outlinePast, 
-        !this.outlinePast, 
+        !this.outlineContact,
+        !this.outlineSkills,
+        !this.outlineCurrently,
+        !this.outlinePast,
         !this.outlineBacklog]
     },
     pastClick: function (event) {
       this.outlinePast = !this.outlinePast
       this.expands = [
-        !this.outlineSkills, 
-        !this.outlineCurrently, 
-        !this.outlinePast, 
-        !this.outlinePast, 
-        !this.outlinePast, 
+        !this.outlineContact,
+        !this.outlineSkills,
+        !this.outlineCurrently,
+        !this.outlinePast,
         !this.outlineBacklog]
     },
     backlogClick: function (event) {
       this.outlineBacklog = !this.outlineBacklog
       this.expands = [
-        !this.outlineSkills, 
-        !this.outlineCurrently, 
-        !this.outlinePast, 
-        !this.outlinePast, 
-        !this.outlinePast, 
+        !this.outlineContact,
+        !this.outlineSkills,
+        !this.outlineCurrently,
+        !this.outlinePast,
         !this.outlineBacklog]
     },
+    contactClick: function (event) {
+      this.outlineContact = !this.outlineContact
+      this.expands = [
+        !this.outlineContact,
+        !this.outlineSkills,
+        !this.outlineCurrently,
+        !this.outlinePast,
+        !this.outlineBacklog]
+    }
   }
 }
 </script>
